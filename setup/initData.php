0e3838b5418a2f8deb347a7cda763698865214b4
Gaël PORT <gael.port@rbs.fr>
2012-09-20 09:16:52 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -11,16 +11,4 @@ class useractionlogger_Setup extends object_InitDataSetup
 		useractionlogger_ModuleService::getInstance()->addPurgeLogTask();
 		useractionlogger_ModuleService::getInstance()->importAllInitScript();
 	}
-
-	/**
-	 * @return array<string>
-	 */
-	public function getRequiredPackages()
-	{
-		// Return an array of packages name if the data you are inserting in
-		// this file depend on the data of other packages.
-		// Example:
-		// return array('modules_website', 'modules_users');
-		return array();
-	}
 }
